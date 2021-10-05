e31269d820534ee536d45e15d5c1de294da2adf6
Matthew Casey <mattcasey879@gmail.com>
2021-10-04 20:03:19 -0500

    finished MVP

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const utils = require("./index");
 describe("[Exercise 1] trimProperties", () => {
   test("[1] returns an object with the properties trimmed", () => {
     // EXAMPLE
-<<<<<<< HEAD
+
     const input = { foo: "  foo ", bar: "bar ", baz: " baz" };
     const expected = { foo: "foo", bar: "bar", baz: "baz" };
     const actual = utils.trimProperties(input);
@@ -15,20 +15,7 @@ describe("[Exercise 1] trimProperties", () => {
     expect(input).toEqual({ foo: "  foo ", bar: "bar ", baz: " baz" });
   });
 });
-=======
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
-    const actual = utils.trimProperties(input)
-    expect(actual).toEqual(expected)
-  })
-   test('[2] returns a copy, leaving the original object intact', () => {
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const actual = utils.trimProperties(input)
-    expect(input).toEqual({ foo: '  foo ', bar: 'bar ', baz: ' baz' })
-   })
 
-})
->>>>>>> 1f1334ad3a19b4d7662af16b1faaa34a6d31a08f
 
 describe("[Exercise 2] trimPropertiesMutation", () => {
   test("[3] returns an object with the properties trimmed", () => {
@@ -123,7 +110,8 @@ describe("[Exercise 6] Car", () => {
   });
    test('[15] driving the car returns the updated odometer', () => {
      focus.drive(100)
-     expect(focus.odometer).toBe(100)
+     focus.drive(100)
+     expect(focus.odometer).toBe(200)
    })
    test('[16] driving the car uses gas', () => {
      focus.drive(100)
@@ -141,6 +129,12 @@ describe("[Exercise 6] Car", () => {
 });
 
 describe("[Exercise 7] isEvenNumberAsync", () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+   test('[19] resolves true if passed an even number', async () => {
+      const result = await utils.isEvenNumberAsync(2)
+      expect(result).toBe(true)
+   })
+   test('[20] resolves false if passed an odd number', async () => {
+    const result = await utils.isEvenNumberAsync(3)
+    expect(result).toBe(false)
+   })
 });
